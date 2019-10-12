98c8b7688664136e727a3335833a45187ad3a54c
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-12 16:19:04 -0300

    Adição do bootstrap e auth e criação do index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('body')
-    <h1>Hello World!</h1>
+    <div class="jumbotron bg-light border-secondary">
+        <div class="row">
+            <div class="card-deck">
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de produtos</h5>
+                        <p class="card-text">
+                            Aqui você cadastra todos os seus produtos. 
+                            Só não se esqueça de cadastrar previamente as categorias.
+                        </p>
+                        <a href="/products" class="btn btn-primary">Cadastre seus produtos</a>
+                    </div>
+                </div>
+
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadastro de categorias</h5>
+                        <p class="card-text">
+                            Aqui vc cadastra todos as suas categorias
+                        </p>
+                        <a href="/categories" class="btn btn-primary">Cadastre suas categorias</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
