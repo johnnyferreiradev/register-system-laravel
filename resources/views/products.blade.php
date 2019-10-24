cd22b73fd84076b27fe7ef2542b6f9c64b6e7576
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-24 11:09:16 -0300

    Criação da estrutura do modal para cadastro de produtos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,49 +1,94 @@
 @extends('layouts.app', ['current'=>'products'])
 
 @section('body')
-<div class="card border">
+    <div class="card border">
         <div class="card-body">
             <h5 class="card-title"> Cadastro de produtos </h5>
-                <a href="/products/new" class="btn btn-sm btn-primary my-3" role="button">Novo produto</a>
-                @if(count($products) > 0)
-                    <table class="table table-ordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Nome</th>
-                                <th>Quantidade</th>
-                                <th>Preço</th>
-                                <th>Categoria</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($products as $product)
-                                <tr>
-                                    <td>{{ $product->id }}</td>
-                                    <td>{{ $product->name }}</td>
-                                    <td>{{ $product->stock }}</td>
-                                    <td>{{ $product->price }}</td>
-                                    <td>{{ $product->categoryName }}</td>
-                                    <td>
-                                        <a
-                                            href="/products/edit/{{$product->id}}"
-                                            class="btn btn-sm btn-primary">
-                                            Editar
-                                        </a>
-                                        <a
-                                            href="/products/delete/{{$product->id}}"
-                                            class="btn btn-sm btn-danger">
-                                            Excluir
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                @else
-                    <h5><strong>Não há produtos cadastrados</strong></h5>
-                @endif
+
+            <table class="table table-ordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Preço</th>
+                        <th>Categoria</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-sm btn-primary" role="button">Novo produto</button>
+        </div>
+    </div>
+
+    <!-- Modal para exibição de formulário de cadastro -->
+    <div class="modal" tabindex="-1" role="dialog" id="dlgProducts">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form class="form-horizontal" id="formProduct">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Novo produto</h5>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="id">
+
+                        <div class="form-group">
+                            <label for="productName" class="control-label">Nome do produto</label>
+                            <div class="input-group">
+                                <input
+                                    type="text" 
+                                    class="form-control" 
+                                    id="productName" 
+                                    placeholder="Nome do produto"
+                                >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="productPrice" class="control-label">Preço</label>
+                            <div class="input-group">
+                                <input
+                                    type="number" 
+                                    class="form-control" 
+                                    id="productPrice" 
+                                    placeholder="Preço do produto"
+                                >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="productStock" class="control-label">Quantidade</label>
+                            <div class="input-group">
+                                <input
+                                    type="number" 
+                                    class="form-control" 
+                                    id="productStock" 
+                                    placeholder="Quantidade em estoque"
+                                >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="productName" class="control-label">Nome do produto</label>
+                            <div class="input-group">
+                                <select id="category" class="form-control">
+                                    <!-- Os options são adicionados via JS -->
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Salvar</button>
+                            <button type="cancel" class="btn btn-secondary" data-dissmiss="modal">Cancelar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
